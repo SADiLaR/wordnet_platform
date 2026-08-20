92560ff396154d113709c7375a7ce4365de98041
Laurette Marais <laurette.p@gmail.com>
2026-08-20 11:46:42 +0200

    Add and wire up view for browsing synsets

--- app/editor/urls.py
+++ app/editor/urls.py
@@ -5,5 +5,14 @@ from . import views
 app_name = "editor"
 
 urlpatterns = [
-    path("", views.landing, name="landing"),
+    path("queue/", views.assignment_queue, name="assignment_queue"),
+    path(
+        "queue/<int:wn_pk>/", views.assignment_queue, name="assignment_queue_by_wordnet"
+    ),
+    path("synsets/", views.browse_synsets, name="browse_synsets"),
+    path(
+        "synsets/<int:wn_pk>/",
+        views.browse_synsets,
+        name="browse_synsets_by_wordnet",
+    ),
 ]
--- app/editor/views.py
+++ app/editor/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from lex.models import Synset, Wordnet
 
 
-def landing(request):
-    return render(request, "editor/landing.html")
+def assignment_queue(request, wn_pk=None):
+    wordnets = Wordnet.objects.all()
+    context = {"wordnets": wordnets}
+    if wn_pk:
+        wordnet_obj = get_object_or_404(Wordnet, pk=wn_pk)
+        context["wordnet"] = wordnet_obj
+    return render(request, "editor/assignment_queue.html", context)
+
+
+def browse_synsets(request, wn_pk=None):
+    wordnets = Wordnet.objects.all()
+    context = {"wordnets": wordnets}
+    if wn_pk:
+        wordnet_obj = get_object_or_404(Wordnet, pk=wn_pk)
+        synsets = Synset.objects.filter(wordnet=wordnet_obj).select_related(
+            "copied_from"
+        )
+        context["wordnet"] = wordnet_obj
+        context["synsets"] = synsets
+
+    return render(
+        request,
+        "editor/browse/synsets.html",
+        context,
+    )

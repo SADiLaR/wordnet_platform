61169830fe1538b1e7ab92055f42373de8827e14
Laurette Marais <laurette.p@gmail.com>
2026-09-09 10:56:48 +0200

    Add synset detail view
    Attempt to show a source synset if available

--- app/wordnet_platform/settings.py
+++ app/wordnet_platform/settings.py
@@ -214,3 +214,7 @@ STORAGES = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Project-specific settings
+
+SOURCE_WORDNET_ID = 1

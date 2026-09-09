5c59db52902a6bbf0a0161b6317b08d47cd7fa94
Laurette Marais <laurette.p@gmail.com>
2026-09-09 10:56:48 +0200

    Add synset detail view
    Attempt to show a source synset if available

--- app/editor/urls.py
+++ app/editor/urls.py
@@ -9,10 +9,15 @@ urlpatterns = [
     path(
         "queue/<int:wn_pk>/", views.assignment_queue, name="assignment_queue_by_wordnet"
     ),
-    path("synsets/", views.browse_synsets, name="browse_synsets"),
+    path("browse/synsets/", views.browse_synsets, name="browse_synsets"),
     path(
-        "synsets/<int:wn_pk>/",
+        "browse/synsets/<int:wn_pk>/",
         views.browse_synsets,
         name="browse_synsets_by_wordnet",
+    ),
+    path(
+        "synsets/<int:ss_pk>/",
+        views.synset_detail,
+        name="synset_detail",
     ),
 ]
--- app/editor/views.py
+++ app/editor/views.py
@@ -1,6 +1,32 @@
+from collections import defaultdict
+
+from django.conf import settings
+from django.db.models import prefetch_related_objects
 from django.shortcuts import get_object_or_404, render
 
-from lex.models import Synset, Wordnet
+from lex.models import Relation, Sense, Synset, Wordnet
+
+
+def _guess_princeton_id(id_code):
+    if id_code and id_code.startswith("ENG20-"):
+        return id_code[6:]
+    return id_code
+
+
+def _guess_source_synset(synset_obj):
+    guessed_id = _guess_princeton_id(synset_obj.princeton_id)
+    if guessed_id:
+        source_synset = (
+            Synset.objects.filter(
+                princeton_id=guessed_id,
+                wordnet_id=settings.SOURCE_WORDNET_ID,  # TODO: make source wordnet configurable
+            )
+            .exclude(pk=synset_obj.pk)
+            .first()
+        )
+    else:
+        source_synset = None
+    return source_synset
 
 
 def assignment_queue(request, wn_pk=None):
@@ -26,5 +52,59 @@ def browse_synsets(request, wn_pk=None):
     return render(
         request,
         "editor/browse/synsets.html",
+        context,
+    )
+
+
+def synset_detail(request, ss_pk):
+
+    synset_obj = get_object_or_404(
+        Synset.objects.select_related("pos", "wordnet"), pk=ss_pk
+    )
+    senses = (
+        Sense.objects.filter(synset=ss_pk)
+        .select_related("word")
+        .prefetch_related("senseexample_set")
+    )
+
+    outgoing_relations = Relation.objects.filter(synset_from=ss_pk).select_related(
+        "type", "synset_to"
+    )
+    incoming_relations = Relation.objects.filter(synset_to=ss_pk).select_related(
+        "type", "synset_from"
+    )
+
+    relations = defaultdict(lambda: {"outgoing": [], "incoming": []})
+    for rel in outgoing_relations:
+        relations[rel.type.name]["outgoing"].append(rel)
+
+    for rel in incoming_relations:
+        relations[rel.type.name]["incoming"].append(rel)
+
+    if synset_obj.copied_from_id:
+        source_synset = Synset.objects.select_related("wordnet").get(
+            pk=synset_obj.copied_from_id
+        )
+        explicit_source = True
+    else:
+        source_synset = _guess_source_synset(synset_obj)
+        explicit_source = False
+
+    if source_synset:
+        prefetch_related_objects(
+            [source_synset], "sense_set__word", "sense_set__senseexample_set"
+        )
+
+    context = {
+        "synset": synset_obj,
+        "senses": senses,
+        "relations": dict(relations),
+        "source_synset": source_synset,
+        "explicit_source": explicit_source,
+    }
+
+    return render(
+        request,
+        "editor/synset_detail.html",
         context,
     )
